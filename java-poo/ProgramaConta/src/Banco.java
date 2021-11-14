51d30bccb589803aa41d5b63fe14f36e48b02cf2
mr-cerebro <mrcerebro8@gmail.com>
2021-11-14 10:46:08 +0100

    Atualizar o projeto

--- java-poo/ProgramaConta/src/Banco.java
+++ java-poo/ProgramaConta/src/Banco.java
@@ -2,35 +2,9 @@ class Banco {
     public static void main(String[] args) {
 
         // Criar os objetos para class Conta()
-        Conta c1 = new Conta();
-        Conta c2 = new Conta();
+        Conta c1 = new Conta("Paulo Pessoa", 123, 10.000);
 
-        // Atribuir os respectivos valores
-        c1.titularConta = "Paulo Pessoa";
-        c1.numConta = 12345;
-        c1.saldoConta = 0.0;
 
-        c2.titularConta = "Catarina Pessoa";
-        c2.numConta = 123456;
-        c2.saldoConta = 0.0;
-
-        // Depositar
-        c1.depositar(100000);
-        c2.depositar(20000);
-
-        // Imprimir
         c1.impimirDados();
-        c2.impimirDados();
-
-        // Sacar
-        System.out.println();
-        System.out.print("CONTA 1 - ");
-        c1.sacar(70000);
-        System.out.print("CONTA 2 - ");
-        c2.sacar(25000);
-
-        // Imprimir novamente
-        c1.impimirDados();
-        c2.impimirDados();
     }
 }
--- java-poo/ProgramaConta/src/Conta.java
+++ java-poo/ProgramaConta/src/Conta.java
@@ -1,7 +1,27 @@
 class Conta {
+    boolean estadoConta;
     String titularConta;
     int numConta;
     double saldoConta;
+
+    //Criar construtor sem parâmetro
+    Conta() {
+        estadoConta = true;
+    }
+
+    //Criar construtor com parâmetro
+    Conta(String nomeConta, int n_Conta) {
+        estadoConta = true;
+        titularConta = nomeConta;
+        numConta = n_Conta;
+    }
+
+    Conta(String nomeConta, int n_Conta, double valorConta) {
+        estadoConta = true;
+        titularConta = nomeConta;
+        numConta = n_Conta;
+        saldoConta = valorConta;
+    }
 
     void depositar(double valorDepositar){
         saldoConta += valorDepositar;
@@ -25,6 +45,7 @@ class Conta {
     void impimirDados(){
         System.out.println();
         System.out.println("DADOS DA CONTA");
+        System.out.println("Estado da conta: " + estadoConta);
         System.out.println("Nome do titular da conta: " + titularConta);
         System.out.println("Número da conta: " + numConta);
         System.out.println("Saldo da conta: " + saldoConta);
